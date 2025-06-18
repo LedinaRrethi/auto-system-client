73cbce1efe18278635528c5ed6413233f90145d7
Ledina Rrethi <ledinarrethi@gmail.com>
2025-06-18 21:24:31 +0200

    feat(FineFilterModal): add alert for filter application and clear filters functionality

--- src/features/police/view/FineRegistrationPage.tsx
+++ src/features/police/view/FineRegistrationPage.tsx
@@ -9,51 +9,36 @@ import { FineCreate } from "../../../types/Fine/FineCreate";
 import { createFine } from "../../../services/fineService";
 
 export default function FineRegistrationPage() {
-  const [filters, setFilters] = useState<FineFilter>({});
+  const [filters, setFilters] = useState<FineFilter>({} as FineFilter);
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleAddClick = () => {
     setModalOpen(true);
   };
 
- const handleModalSubmit = async (data: FineCreate): Promise<boolean> => {
-  try {
-    await createFine(data);
-    setModalOpen(false);
-    return true;
-  } catch (err) {
-    console.error("Error creating fine:", err);
-    return false;
-  }
-};
-
+  const handleModalSubmit = async (data: FineCreate): Promise<boolean> => {
+    try {
+      await createFine(data);
+      setModalOpen(false);
+      return true;
+    } catch (err) {
+      console.error("Error creating fine:", err);
+      return false;
+    }
+  };
 
   return (
     <>
-      <PageMeta
-        title="Fine Registration | AutoSystem"
-        description="Manage and monitor fines."
-      />
+      <PageMeta title="Fine Registration | AutoSystem" description="Manage and monitor fines." />
       <PageBreadcrumb pageTitle="Fine Registration" />
 
       <div className="space-y-6">
-        <ComponentCard
-          title="Fine registration"
-          desc="Here you can add fines, search and filter."
-        >
-          <FineRegistrationTable
-            onAdd={handleAddClick}
-            filters={filters}
-            onFilterChange={setFilters}
-          />
+        <ComponentCard title="Fine registration" desc="Here you can add fines, search and filter.">
+          <FineRegistrationTable onAdd={handleAddClick} filters={filters} onFilterChange={setFilters} />
         </ComponentCard>
       </div>
 
-      <FineRegistrationModal
-        isOpen={modalOpen}
-        onClose={() => setModalOpen(false)}
-        onSubmit={handleModalSubmit}
-      />
+      <FineRegistrationModal isOpen={modalOpen} onClose={() => setModalOpen(false)} onSubmit={handleModalSubmit} />
     </>
   );
 }
