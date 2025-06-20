c7ed985ea6b7984adf06c56e7c72248d284f03a7
Ledina Rrethi <ledinarrethi@gmail.com>
2025-06-20 17:07:09 +0200

    feat(InspectionForm): implement useInspectionForm hook to manage inspection requests and data fetching

--- src/services/directoryService.ts
+++ src/services/directoryService.ts
@@ -1,6 +1,5 @@
 import api from "./api";
 
-// Merr listen e drejtorive nga backend
 export async function getDirectorates() {
   const response = await api.get("/Directorate");
   return response.data;
