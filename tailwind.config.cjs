8d097283cce3f3e1f71242d319900c0076595027
Ledina Rrethi <ledinarrethi@gmail.com>
2025-06-26 16:49:38 +0200

    - Removed flowbite and flowbite-react from package.json dependencies. - Updated tailwind.config.cjs to remove flowbite-related content paths and plugins. - Modified vite.config.ts to eliminate flowbite-react plugin integration.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}", 
-    "node_modules/flowbite/**/*.js",                    
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin")                           
-  ],
 };
