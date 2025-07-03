a472c3985c550f09a9778132c4901fbdb3fbdf29
Ledina Rrethi <ledinarrethi@gmail.com>
2025-07-03 14:31:52 +0200

    fix: update FineRegistrationTable to use index as fallback for TableRow key

--- src/features/police/components/FineRegistrationTable.tsx
+++ src/features/police/components/FineRegistrationTable.tsx
@@ -137,8 +137,8 @@ export default function FineRegistrationTable({
 
           
           <TableBody>
-            {fines.map((fine) => (
-              <TableRow key={fine.idpk_Fine}>
+            {fines.map((fine , index) => (
+              <TableRow key={fine.idpk_Fine ?? index}>
                 <TableCell className="px-5 py-4 text-sm text-gray-700 dark:text-white">
                   {fine.plateNumber}
                 </TableCell>
